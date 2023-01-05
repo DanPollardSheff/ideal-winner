eb714d34f7d4249e6bda699e0a496797d8983136
Dan Pollard <d.j.pollard@sheffield.ac.uk>
2023-01-05 10:20:07 +0000

    tied up the model script and the .csv files. Moved all global conrol variables out of the main model run script and put it into Set Global variables.R. This script is now called by the Master major trauma model file. Moved all .csv files relating to setting the model population into the Population folder. Moved all.csv files relating to model parameters into the Paramereters folder. File paths calling these files within the Master major trauma model file.R have been updated.

--- Master major trauma model file.R
+++ Master major trauma model file.R
@@ -6,65 +6,21 @@ library(MASS)
 library(parallel)
 library(doParallel)
 
-numCores <- (detectCores() -1)  #Leave 1 Core for OS
+numCores <- (detectCores() -1)  #Number of cores avaialable minus 1, to
+                                #leave 1 Core for OS
 
-#Global variables
-PSA_switch <- 1                 #1=run PSA, 0=deterministic
-PSA_numb <- 2                   #number of PSA runs
-pat_numb <- 25000               #number of patients
-days_to_discharge <- 30         #number of days to discharge from hospital
-days_in_year <- 365.25          #number of days in a year
-time_horizon <- 100             #time horizon, years
-discount_rate_QALYs <- 0.035    #discount rate, QALYs
-discount_rate_costs <- 0.035    #discount rate, costs
-Param_export <- 1               #1=save a copy of PSA parameters
-
-#The proportion of clinical benefit that patients with an ISS of over 8 and under 16 receive
-#compared to people with an ISS of 16 or more (0 to 1). Default is no benefit (0)
-Proportion_RR_MTC_ISS_o8_u16_hosp <- 0
-Proportion_RR_MTC_ISS_o8_u16_1yr <- 0
-
-#The proportion of clinical benefit that patients who are initially sent to a non-MTC receive
-#compared to people sent straight to an MTC (0 to 1). Default is full benefit (1)
-Proportion_RR_MTC_transfer_hosp <- 1
-Proportion_RR_MTC_ISS_transfer_1yr <- 1
-
-
-TARN_mort_eq <- "Old"           #options are new or old. Default is old
-MTCs_in_mort_risk <- "No"       #options are Yes or no. Relates to whether the mort eq is a composite risk score for a 
-
-#population who has / has not been to an MTC or a population who hasn't gone to an MTC. Default is no, as the
-#default for the  mortality equation is the Old TARN equation.
-percent_TARN_cases_reported_ISS_o16 <- 1
-percent_TARN_cases_reported_ISS_o9_u16 <- 1
-
-population_source <- "Dutch" #Source of simulated population.Options are UK, Dutch, Dutch_simp. Dutch is the default
-
-population_ISS_over16_only <- "No"  #Option for resampling to produce a population with ISS >= 16. Options are "Yes" or "No". Default is no. 
-population_ISS_under16_only <- "No" #Option for resampling to produce a population with ISS < 16. Can be "Yes" or "No". Default is no.
-
-efficent_life_expectancy <- "Yes"   #Options are Yes or No. Default is yes
-
-test_pat_chars <- "No"              #Change this to Yes if you only want to run the base case analysis with patient level results
-
-PSA_rand_no <-  -99                 #random number to determine PSA parameters either -99 (to not reset the seed) or any positive number
-
-date <- "_1_75_ben_ISS_u9"          #name to append to saved files 
-
-Eldery_specific_params  <- T          #Takes value T or F. If T model has different parameters for
-                                      #elderly (65+) populations
-Pead_specific_params    <- T          #Takes value T or F. If T the model has different parameters
-                                      #for peadatric (14 and under) population
-TARN_22_params          <- T          #Use the TARN 22 parameter estimates? T = TRUE, F = FALSE
+#read in the r script that sets the global variables
+source("Set global variables.R")
 
 #read in files / save files from the X drive (note not on Git due to confidentiality reasons)
 file_location <- "\\\\uosfstore.shefuniad.shef.ac.uk\\shared\\ScHARR\\PR_MATTS\\General\\Health Economics\\Phase 1  & 2\\Model\\"
 
-param_data <- read.csv("parameters.csv", row.names=1)
-tarn_22_means <- read.csv("New TARN Means.csv", row.names = 1)
-tarn_22_vcov <- read.csv("New TARN vcov matrix.csv", row.names=1)
-life_tabs <- read.csv("ONSlifetables.csv")
-future_costs <- read.csv("lifetime-healthcare-costs.csv")
+#read in global data files
+param_data <- read.csv("Parameters/parameters.csv", row.names=1)
+tarn_22_means <- read.csv("Parameters/New TARN Means.csv", row.names = 1)
+tarn_22_vcov <- read.csv("Parameters/New TARN vcov matrix.csv", row.names=1)
+life_tabs <- read.csv("Parameters/ONSlifetables.csv")
+future_costs <- read.csv("Parameters/lifetime-healthcare-costs.csv")
 
 if(population_source=="UK"){
   means <- as.matrix(read.csv(paste(file_location,"means.csv", sep=""),row.names=1))
@@ -74,13 +30,13 @@ if(population_source=="UK"){
   ISS_tab <- read.csv(paste(file_location,"ISS_tab.csv", sep=""),row.names=1)
   GCS_tab <- read.csv(paste(file_location,"GCS_tab.csv", sep=""),row.names=1)
 }else if (population_source== "Dutch_simp"){
-  means <- as.matrix(read.csv("means_dutch_v2.csv",row.names=1))
-  covariance <- as.matrix(read.csv("covariance_dutch_v2.csv", row.names=1))
-  age_tab <- read.csv("age_tab_dutch_v2.csv",row.names=1)
-  gen_tab <- read.csv("male_tab_dutch_v2.csv",row.names=1)
-  ISS_tab <- read.csv("ISS_tab_dutch_v2.csv",row.names=1)
-  GCS_tab <- read.csv("GCS_tab_dutch_v2.csv",row.names=1)
-  blunt_tab <- read.csv("blunt_tab_dutch_v2.csv",row.names=1)
+  means <- as.matrix(read.csv("Population/means_dutch_v2.csv",row.names=1))
+  covariance <- as.matrix(read.csv("Population/covariance_dutch_v2.csv", row.names=1))
+  age_tab <- read.csv("Population/age_tab_dutch_v2.csv",row.names=1)
+  gen_tab <- read.csv("Population/male_tab_dutch_v2.csv",row.names=1)
+  ISS_tab <- read.csv("Population/ISS_tab_dutch_v2.csv",row.names=1)
+  GCS_tab <- read.csv("Population/GCS_tab_dutch_v2.csv",row.names=1)
+  blunt_tab <- read.csv("Population/blunt_tab_dutch_v2.csv",row.names=1)
 }else{
   means <- as.matrix(read.csv(paste(file_location,"means_dutch_v2.csv", sep=""), row.names=1))
   covariance <- as.matrix(read.csv(paste(file_location,"covariance_dutch_v2.csv", sep=""), row.names=1))
